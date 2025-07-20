0356fafd92b6f4c486bc8ab9ac53c594cf5499e4
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-07-20 17:46:13 -0400

    Add if else logic for computer choice

--- rps-scripts.js
+++ rps-scripts.js
@@ -1,3 +1,11 @@
-
+var str1
 let randomNumber=Math.floor(Math.random() * 100) +1;
 console.log(randomNumber);
+if (randomNumber <= 33) {
+    str1 = "rock";
+} else if (randomNumber >= 66) {
+    str1 = "paper";
+} else {
+    str1 = "scissors"
+}
+console.log(str1);
