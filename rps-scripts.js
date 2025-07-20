e94f95afe8784d45945dd79c8f1b1638f88b37d9
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-07-20 18:12:08 -0400

    Added getComputerChoice function

--- rps-scripts.js
+++ rps-scripts.js
@@ -1,11 +1,14 @@
 var str1
-let randomNumber=Math.floor(Math.random() * 100) +1;
-console.log(randomNumber);
-if (randomNumber <= 33) {
-    str1 = "rock";
-} else if (randomNumber >= 66) {
-    str1 = "paper";
-} else {
-    str1 = "scissors"
+function getComputerChoice() {
+    randomNumber=Math.floor(Math.random() * 100) +1;
+    console.log(randomNumber);
+    if (randomNumber <= 33) {
+        str1 = "rock";
+    } else if (randomNumber >= 66) {
+        str1 = "paper";
+    } else {
+        str1 = "scissors"
+    }
+    return str1;
 }
-console.log(str1);
+console.log(getComputerChoice());
