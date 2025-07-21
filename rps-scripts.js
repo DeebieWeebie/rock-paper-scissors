6ca184b3b10a8bd320858ead98053cd1cc6c207d
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-07-20 21:07:42 -0400

    Change str var names & started on playRound logic
    Str1 & str2 changed to computer Choice and human Choice

--- rps-scripts.js
+++ rps-scripts.js
@@ -1,23 +1,42 @@
-var str1
+var computerChoice
+var humanScore = 0
+var computerScore = 0
+
 function getComputerChoice() {
     randomNumber=Math.floor(Math.random() * 100) +1;
     console.log(randomNumber);
     if (randomNumber <= 33) {
+        computerChoice = "rock";
+    } else if (randomNumber >= 66) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors"
+    }
+    return computerChoice;
+}
+console.log(getComputerChoice());
+
+var input
+var humanChoice
+
+function getHumanChoice() {
+    input = prompt("Please enter your choice /(either rock, paper, or scissors/): ");
+    return humanChoice = input.toLowerCase();
+}
+console.log(getHumanChoice());
+
+function playRound(humanChoice,computerChoice) {
+    if (humanChoice == computerChoice) {
         str1 = "rock";
     } else if (randomNumber >= 66) {
         str1 = "paper";
     } else {
         str1 = "scissors"
     }
-    return str1;
+    return
 }
-console.log(getComputerChoice());
 
-var input
-var str2
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-function getHumanChoice() {
-    input = prompt("Please enter your choice /(either rock, paper, or scissors/): ");
-    return str2 = input.toLowerCase();
-}
-console.log(getHumanChoice());
+playRound(humanSelection, computerSelection);
