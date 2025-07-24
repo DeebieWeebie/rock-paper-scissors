c4dec49f70e0cf8aa9a48d3b1d4b729d290f5821
DeebieWeebie <120836210+DeebieWeebie@users.noreply.github.com>
2025-07-23 21:11:53 -0400

    Move playRound to playGame.
    Add iterator.  Now messages don't pass correctly.

--- rps-scripts.js
+++ rps-scripts.js
@@ -1,12 +1,12 @@
-let humanScore = 0;
-let computerScore = 0;
-let computerChoice = "";
-let humanChoice = "";
-let input = ""
-let userMessage = "";
+let computerChoice;
+let humanChoice;
+let input;
+let userMessage;
+let scoreMessage;
+let i;
 
 function getComputerChoice() {
-    randomNumber=Math.floor(Math.random() * 100) +1;
+    randomNumber = Math.floor(Math.random() * 100) + 1;
     console.log(randomNumber);
     if (randomNumber <= 33) {
         return computerChoice = "rock";
@@ -19,41 +19,59 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     input = prompt("Please enter your choice /(either rock, paper, or scissors/): ");
-    return humanChoice = input.toLowerCase(); 
-}
-
-function playRound(humanChoice, computerChoice) {
-    console.log(computerChoice);
-    console.log(humanChoice);
-    if (humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore + 1;
-        return userMessage = "You win! Rock beats scissors!";
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore + 1;
-        return userMessage = "You win! Paper beats rock!";
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore + 1;
-        return userMessage = "You win! Scissors beats paper!";
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        computerScore + 1;
-        return userMessage = "You lose! Paper beats rock!";
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        computerScore + 1;
-        return userMessage = "You lose! Scissors beats paper!";
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        computerScore + 1;
-        return userMessage = "You lose! Rock beats scissors!";
-    } else {
-        return userMessage = "It's a tie!  We are playing best of 5.  Play again!"
-    }
+    humanChoice = input.toLowerCase();
+    return humanChoice;
 }
 
 const computerSelection = getComputerChoice();
 const humanSelection = getHumanChoice();
 
-playRound(humanSelection, computerSelection);
-
 function playGame() {
-    console.log(userMessage)
+    let humanScore;
+    let computerScore;
+    console.log(computerChoice);
+    console.log(humanChoice);
+
+    for (let i = 0; i < 5; i++) {   
+        
+        function playRound(humanChoice, computerChoice) {  
+            if (humanChoice === "rock" && computerChoice === "scissors") {
+                humanScore = humanScore + 1;
+                return userMessage = "You win! Rock beats scissors!";
+            } else if (humanChoice === "paper" && computerChoice === "rock") {
+                humanScore++;
+                return userMessage = "You win! Paper beats rock!";
+            } else if (humanChoice === "scissors" && computerChoice === "paper") {
+                humanScore++;
+                return userMessage = "You win! Scissors beats paper!";
+            } else if (humanChoice === "rock" && computerChoice === "paper") {
+                computerScore++;
+                return userMessage = "You lose! Paper beats rock!";
+            } else if (humanChoice === "paper" && computerChoice === "scissors") {
+                computerScore++;
+                return userMessage = "You lose! Scissors beats paper!";
+            } else if (humanChoice === "scissors" && computerChoice === "rock") {
+                computerScore++;
+                return userMessage = "You lose! Rock beats scissors!";
+            } else if (humanChoice === computerChoice) {
+                return userMessage = "It's a tie!  We are playing best of 5.  Play again!"
+            }
+        }
+//work on everything below this and figure out the message stuff again//       
+    playRound (i);
+    console.log(userMessage);
+    console.log(scoreMessage);
+        return scoreMessage = `The score is the computer ${computerScore} vs you: ${humanScore}`;
+    }
+
+    if (i === 5 && computerScore > humanScore) {
+        scoreMessage = `The winner is the computer!  The computer's final score is: ${computerScore} and your final score is: ${humanScore}`;
+    } else if (i === 5 && humanScore > computerScore) {
+        scoreMessage = `The winner is you! Your final score is: ${humanScore} and the computer's final score is: ${computerScore}`;
+    } else {
+        scoreMessage = "It's a tie!  Play 5 rounds again!"
+    }
+    console.log(scoreMessage);
 }
-playGame();
+
+playGame(i);
